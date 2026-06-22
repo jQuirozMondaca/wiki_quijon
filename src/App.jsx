4c5a89069e3d273a4025029aac464557c7a75f81
jQuirozMondaca <128020518+jQuirozMondaca@users.noreply.github.com>
2026-06-22 19:51:31 -0400

    cambio de titulo

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>MI Wiki con amor</h1>
+          <h1>MI Wiki QuiJon</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
